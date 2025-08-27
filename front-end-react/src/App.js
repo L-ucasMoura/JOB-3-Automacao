7321d9967fa7237c11b1e02e2012ac4d46b44cad
Lucas de Amorim Moura <lucas.amorimmoura@ufpe.br>
2025-08-27 11:54:12 -0300

    Adição do Front-end

---
+++ front-end-react/src/App.js
@@ -0,0 +1,35 @@
+// Assim que comenta!
+import logo from './logo.svg';
+import './App.css';
+//Hooks mais importantes: UseState e UseEffect
+import {useEffect, useState} from 'react';
+
+
+function App() {
+  const [data, setData] = useState('Meu valor inicial');
+
+  useEffect(() => {
+    setData('Meu novo valor');
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Oh my...
+          Minha variável de estado vale: {data}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
