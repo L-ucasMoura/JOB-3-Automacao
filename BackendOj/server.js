5c0ad06d047fb0835fb6da0a065c9e99b13643ea
gabrielojaimi <gabriel.ojaimi@ufpe.br>
2025-09-01 02:15:18 -0300

    fix( Backend)
    Correção package.json

--- BackendOj/server.js
+++ BackendOj/server.js
@@ -110,6 +110,6 @@ app.get('/usuarios', async (req, res) => {
 })
 
 
-app.listen(3000, () => {
-  console.log('🚀 Servidor rodando em http://localhost:3000')
+app.listen(3001, () => {
+  console.log('🚀 Servidor rodando em http://localhost:3001')
 })
